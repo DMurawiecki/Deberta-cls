dc6b596b0af69117b8caa7ac1afaf59305a15bfe
Daniil Muravetskii <mdr333@mail.ru>
2025-06-06 12:16:31 +0300

    Update download_data.py

--- download_data.py
+++ download_data.py
@@ -8,25 +8,25 @@ def download_data() -> None:
     folder_id_2 = "1FxsuvrSfkPAyKePlwSCDgbIG8BSmkN5O"
     destination_folder_1 = "./"
     destination_folder_2 = "./bot"
-    adresses = [
+    addresses = [
         (destination_folder_1, folder_id_1),
         (destination_folder_2, folder_id_2),
     ]
 
-    for destination_folder, folder_id in adresses:
+    for destination_folder, folder_id in addresses:
         os.makedirs(destination_folder, exist_ok=True)
         folder_url = f"https://drive.google.com/drive/folders/{folder_id}"
         gdown.download_folder(url=folder_url, output=destination_folder, quiet=False)
 
+
 def download_model_from_ckpt() -> None:
     model_url = (
-        "https://drive.google.com/drive/folders/1Jouhh1dowuyycFIYVlMayZvkqYDPL82g"
+        "https://drive.google.com/drive/folders/" "1Jouhh1dowuyycFIYVlMayZvkqYDPL82g"
     )
     destination_folder = "./"
     if not os.path.exists(destination_folder):
-        os.mkdirs(destination_folder, exist_ok=True)
+        os.makedirs(destination_folder, exist_ok=True)
     gdown.download_folder(url=model_url, output=destination_folder, quiet=False)
-)
 
 
 if __name__ == "__main__":
