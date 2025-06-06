ecc41c2cdfcf944af5c8cb4ece61c5dd74a2e0b3
Daniil Muravetskii <mdr333@mail.ru>
2025-06-06 16:49:29 +0300

    Update rag_engine.py

--- bot/rag_engine.py
+++ bot/rag_engine.py
@@ -27,6 +27,7 @@ def get_retriever_and_model():
 
     load_dotenv(override=True)
     api_key = os.environ.get("MISTRAL_API_KEY")
+    print("[DOCKER DEBUG] MISTRAL_API_KEY =", os.environ.get("MISTRAL_API_KEY"))
     if not api_key:
         print("MISTRAL_API_KEY not found in environment variables.")
         sys.exit(1)
