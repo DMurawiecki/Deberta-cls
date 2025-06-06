0322efb2559e2d1de77018abf4fe9813f93bb23f
Daniil Muravetskii <mdr333@mail.ru>
2025-06-06 12:08:39 +0300

    Update download_data.py

--- download_data.py
+++ download_data.py
@@ -18,6 +18,17 @@ def download_data() -> None:
         folder_url = f"https://drive.google.com/drive/folders/{folder_id}"
         gdown.download_folder(url=folder_url, output=destination_folder, quiet=False)
 
+def download_model_from_ckpt() -> None:
+    model_url = (
+        "https://drive.google.com/drive/folders/1Jouhh1dowuyycFIYVlMayZvkqYDPL82g"
+    )
+    destination_folder = "./"
+    if not os.path.exists(destination_folder):
+        os.mkdirs(destination_folder, exist_ok=True)
+    gdown.download_folder(url=model_url, output=destination_folder, quiet=False)
+)
+
 
 if __name__ == "__main__":
     download_data()
+    download_model_from_ckpt()
